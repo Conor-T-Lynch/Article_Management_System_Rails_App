71fdecbbc0bdcaacdf985be46ba8f5b0a2146ee5
conor lynch <ctlynch89@live.com>
2024-11-08 02:12:48 +0000

    Disable forced SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts.clear
+  
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
@@ -51,7 +51,7 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
