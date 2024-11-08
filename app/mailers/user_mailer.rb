f8191f1b8fb8441574ad89a4fd77c7cbd26a76c5
conor lynch <ctlynch89@live.com>
2024-11-08 15:22:17 +0000

    new mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
     def password_reset(user)
       @user = user
       @token = user.reset_password_token
-      @reset_link = edit_user_password_url(reset_password_token: @user.reset_password_token, host: "18.194.204.167")
+      @reset_link = edit_user_password_url(reset_password_token: @user.reset_password_token, host: "Localhost", port: 3000)
       mail(to: @user.email, subject: "Reset Your Password")
     end
 end
