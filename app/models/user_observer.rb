9d3331a3e2153d1d7b3005969c9c8f2a3ce406e5
conor lynch <ctlynch89@live.com>
2024-11-08 18:23:20 +0000

    Mailer

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,15 +1,15 @@
 class UserObserver < ActiveRecord::Observer
   # changes to the user model is being watched by the observer
-  observe :user
+  #observe :user
 
   # password reset notification instructions
-  ActiveSupport::Notifications.subscribe("after_reset_password_instructions") do |*args|
+  #ActiveSupport::Notifications.subscribe("after_reset_password_instructions") do |*args|
     # new event object is being created due to received arguments
-    event = ActiveSupport::Notifications::Event.new(*args)
+    #event = ActiveSupport::Notifications::Event.new(*args)
     # the user object is being extracted due to event's payload
-    user = event.payload
+    #user = event.payload
 
     # logs the users email address to the console, to which the password instructions where sent to
-    puts "Password reset instructions sent to: #{user.email}"
-  end
+    #puts "Password reset instructions sent to: #{user.email}"
+  #end
 end
